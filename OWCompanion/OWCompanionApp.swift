ab06b8121f5ba671dc8c8bd335b76c99bc7601ef
Furkan Turkmen <the.furkan@icloud.com>
2022-07-18 15:16:30 +0200

    Initial Commit

---
+++ OWCompanion/OWCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OWCompanionApp.swift
+//  OWCompanion
+//
+//  Created by Furkan Türkmen on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct OWCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
